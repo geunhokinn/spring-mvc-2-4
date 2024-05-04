3acc1678288f9dc3bd9d457b6749fc06754e5301
geunhokinn <gghh1220@gmail.com>
2024-05-04 18:28:34 +0900

    feat: 오류 발생시 사용자 입력 값 유지 기능 추가

--- src/main/java/hello/itemservicev4/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservicev4/web/validation/ValidationItemControllerV2.java
@@ -40,13 +40,13 @@ public class ValidationItemControllerV2 {
     }
 
     @GetMapping("/add")
-    public String addFormV1(Model model) {
+    public String addForm(Model model) {
         model.addAttribute("item", new Item());
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+//    @PostMapping("/add")
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         // BindingResult : 스프링이 제공하는 검증 오류를 보관하는 객체
         // @ModelAttribute 는 자동으로 model 에 등록함.
 
@@ -74,6 +74,55 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 // 특정 필드랑 비교하기 어려움 -> global 오류
                 bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        // errors 가 있으면
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {} ", bindingResult);
+            // bindingResult 는 자동으로 view 에 넘어감.
+//            model.addAttribute("errors", errors);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // BindingResult : 스프링이 제공하는 검증 오류를 보관하는 객체
+        // @ModelAttribute 는 자동으로 model 에 등록함.
+
+        // 검증 로직
+
+        // 필드 오류 검증
+        // 글자가 없으면
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+        // 가격이 없고 1,000 ~ 1,000,000 이 아니면
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,"가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+        }
+        // 수량이 없고 9,999 이상이면
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // 가격이 있으면서 수량도 있으면
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            // 가격 * 수량이 10,000 보다 작으면
+            if (resultPrice < 10000) {
+                // 특정 필드랑 비교하기 어려움 -> global 오류
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
