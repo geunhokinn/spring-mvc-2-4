e9aec23af9b555aa416c864cfc21c5b67fd7de13
geunhokinn <gghh1220@gmail.com>
2024-05-04 15:31:21 +0900

    feat: v1 프로젝트 생성

---
+++ src/main/java/hello/itemservicev4/ItemServiceV4Application.java
@@ -0,0 +1,13 @@
+package hello.itemservicev4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceV4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceV4Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/itemservicev4/ItemServiceV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemservicev4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceV4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
