4ad71a36e4403882f0a85c1b973d3c0aa39f446d
geunhokinn <gghh1220@gmail.com>
2024-05-05 21:07:11 +0900

    feat: Bean Validation 의 groups 로 등록과 수정 분리 기능 추가

--- src/main/java/hello/itemservicev4/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservicev4/web/validation/ValidationItemControllerV3.java
@@ -2,6 +2,8 @@ package hello.itemservicev4.web.validation;
 
 import hello.itemservicev4.domain.item.Item;
 import hello.itemservicev4.domain.item.ItemRepository;
+import hello.itemservicev4.domain.item.SaveCheck;
+import hello.itemservicev4.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -45,8 +47,43 @@ public class ValidationItemControllerV3 {
         return "validation/v3/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // BindingResult : 스프링이 제공하는 검증 오류를 보관하는 객체
+        // @ModelAttribute 는 자동으로 model 에 등록함.
+        // @Validated 는 Item 에 대해서 검증을 수행한다.
+        // 검증을 하고 bindResult 에 결과 값이 담긴다.
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // 가격이 있으면서 수량도 있으면
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            // 가격 * 수량이 10,000 보다 작으면
+            if (resultPrice < 10000) {
+                // 특정 필드랑 비교하기 어려움 -> global 오류
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        // errors 가 있으면
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {} ", bindingResult);
+            // bindingResult 는 자동으로 view 에 넘어감.
+//            model.addAttribute("errors", errors);
+            return "validation/v3/addForm";
+        }
+
+        // 성공 로직
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         // BindingResult : 스프링이 제공하는 검증 오류를 보관하는 객체
         // @ModelAttribute 는 자동으로 model 에 등록함.
         // @Validated 는 Item 에 대해서 검증을 수행한다.
@@ -87,7 +124,7 @@ public class ValidationItemControllerV3 {
         return "validation/v3/editForm";
     }
 
-    @PostMapping("/{itemId}/edit")
+//    @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
 
         // 특정 필드가 아닌 복합 룰 검증
@@ -114,5 +151,31 @@ public class ValidationItemControllerV3 {
         return "redirect:/validation/v3/items/{itemId}";
     }
 
+    @PostMapping("/{itemId}/edit")
+    public String editV2(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // 가격이 있으면서 수량도 있으면
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            // 가격 * 수량이 10,000 보다 작으면
+            if (resultPrice < 10000) {
+                // 특정 필드랑 비교하기 어려움 -> global 오류
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        // errors 가 있으면
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {} ", bindingResult);
+            // bindingResult 는 자동으로 view 에 넘어감.
+//            model.addAttribute("errors", errors);
+            return "validation/v3/editForm";
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
 }
 
