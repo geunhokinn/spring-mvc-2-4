cbd4741f3fc5c6839ac9bf4f9d75ecbebe6fe150
geunhokinn <gghh1220@gmail.com>
2024-05-05 15:20:37 +0900

    test: 메시지 코드 리졸버 테스트

---
+++ src/test/java/hello/itemservicev4/validation/MessageCodesResolverTest.java
@@ -0,0 +1,30 @@
+package hello.itemservicev4.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        // 메시지 코드를 만들어 줌.
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required");
+    }
+}
